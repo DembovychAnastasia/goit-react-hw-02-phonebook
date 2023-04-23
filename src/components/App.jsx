80e8031c0577b38e79c1f179b7dca93a57e4c165
DembovychAnastasia <113368687+DembovychAnastasia@users.noreply.github.com>
2023-04-23 13:04:47 +0300

    add error alert

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,14 +34,17 @@ export class App extends Component {
       contact =>
         contact.name.toLowerCase().trim() ===
           newContact.name.toLowerCase().trim() ||
-        contact.number.trim() === newContact.number.trim()
+        contact.number.trim() === newContact.number.trim() 
     ).length
-      ? this.error(`${newContact.name}: is already in contacts`)
+      ? alert(`${newContact.name}: is already in contacts`)
       : this.setState(prevState => {
           return {
             contacts: [newContact, ...prevState.contacts],
           };
+          
         });
+
+        
   };
 
   
@@ -63,6 +66,7 @@ export class App extends Component {
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
+  
   changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
   };
